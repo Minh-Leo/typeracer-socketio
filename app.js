ab973c231d71d7fc991185c54162efc646bce7ba
Minh Bui <tranduyminh.bui@gmail.com>
2020-08-31 18:35:36 +1000

    setup socket for createGame and JoinGame

--- app.js
+++ app.js
@@ -11,9 +11,56 @@ const Game = require('./model/Game');
 const QuotableAPI = require('./QuotableAPI');
 
 mongoose.connect(
-  'mondodb://localhost:27017/typeracerTutorial',
+  'mongodb://localhost:27017/typeracerGame',
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
     console.log('successfully connected to db');
   }
 );
+
+io.on('connect', (socket) => {
+  socket.on('create-game', async (nickName) => {
+    try {
+      const quotableData = await QuotableAPI();
+      let game = new Game();
+      game.words = quotableData;
+
+      let player = {
+        socketID: socket.id,
+        isPartyLeader: true,
+        nickName,
+      };
+      game.players.push(player);
+      game = await game.save();
+
+      const gameID = game._id.toString();
+      socket.join(gameID);
+
+      // send updated game to all sockets within game
+      io.to(gameID).emit('update-game', game);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  socket.on('join-game', async ({ gameID: _id, nickName }) => {
+    try {
+      let game = await Game.findById(_id);
+      if (game.isOpen) {
+        const gameID = (await game)._id.toString();
+        socket.join(gameID);
+
+        let player = {
+          socketID: socket.id,
+          nickName,
+        };
+        game.players.push(player);
+        game = await game.save();
+
+        io.to(gameID).emit('update-game', game);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+});
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+
+import history from './history';
+import socket from './socketConfig';
+import GameMenu from './components/GameMenu';
+import CreateGame from './components/CreateGame';
+import JoinGame from './components/JoinGame';
 import './App.css';
 
 function App() {
+  const [gameState, setGameState] = useState({
+    _id: '',
+    isOpen: false,
+    players: [],
+    words: [],
+  });
+
+  useEffect(() => {
+    socket.on('update-game', (game) => {
+      console.log(game);
+      setGameState(game);
+    });
+
+    return () => {
+      socket.removeAllListeners();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (gameState._id !== '') history.push(`/game/${gameState._id}`);
+  }, [gameState._id]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <Switch>
+        <Route exact path='/' component={GameMenu} />
+        <Route path='/game/create' component={CreateGame} />
+        <Route path='/game/join' component={JoinGame} />
+      </Switch>
+    </Router>
   );
 }
 
